5944c167046d77040a63cd46daff7b357881f70c
Unknown <anasirudin.bella@gmail.com>
2019-01-15 17:13:30 +0800

    Numerically add noise using fg module

--- devel/test_series/gridded_noise_nofg.py
+++ devel/test_series/gridded_noise_nofg.py
@@ -11,13 +11,16 @@ Here are the tests:
 
 """
 from base_definitions import CustomCoreInstrument, CustomLikelihood, core_eor, run_mcmc, DEBUG
+from py21cmmc_fg.core import CorePointSourceForegrounds
 
-model_name = "InstrumentalGridTestNoise"
+model_name = "InstrumentalGridTestNoise-numerical-1d"
 
 core_instr = CustomCoreInstrument(
     antenna_posfile = 'grid_centres', # use a special grid of *baselines*.
     Tsys=240,
 )
+# Add foregrounds core but set S_min=S_max so essentially no foregrounds so that we can add noise numerically
+core_fg = CorePointSourceForegrounds(S_min=1e-4, S_max=1e-4, gamma=0)
 
 likelihood = CustomLikelihood(
     datafile=[f'data/{model_name}.npz'],
@@ -26,7 +29,7 @@ likelihood = CustomLikelihood(
 
 if __name__ == "__main__":
     chain = run_mcmc(
-        [core_eor, core_instr], likelihood,
+        [core_eor, core_fg, core_instr], likelihood,
         model_name=model_name,             # Filename of main chain output
     )
 
